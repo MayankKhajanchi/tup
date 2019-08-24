289c92a8163873be8a123d2f80937fb2c62806df
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-08-24 17:33:31 +0530

    index.js removed

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
- $('.image').click(function(event) {
-   const x = $(this).css('background-image');
-   $('#exampleModal').modal('toggle');
-   $('.modal-body').css("background-image", x);
- });
-});
-
